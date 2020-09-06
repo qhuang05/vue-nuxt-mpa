ca2a7caf89d17f74d3645d40d0b857f9fdcb5ed8
qhuang05 <qhuang05@sina.com>
2020-09-06 13:16:44 +0800

    fix bug

---
+++ components/topMenu.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="top-menu">
+        <div>icon</div>
+        <div class="nav-list">
+            <ul>
+                <li>
+                    <a href="">首页</a>
+                </li>
+                <li>
+                    <a href="">设计</a>
+                    <div>
+                        <div>
+                            <div class="tit">设计</div>
+                            <ul>
+                                <li>设计器</li>
+                                <li>批量设计</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a href="">刊登</a>
+                    <div>
+                        <div>
+                            <div class="tit">通用数据</div>
+                            <ul>
+                                <li>定制产品</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <div class="tit">亚马逊</div>
+                            <ul>
+                                <li>定制产品</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+    @import '~/assets/style/variable.scss';
+    .top-menu{
+        background-color: $COLOR_1;
+    }
+</style>
--- layouts/common.vue
+++ layouts/common.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-      <header>
+      <top-menu></top-menu>
+      <!-- <header>
         <nuxt-link to="/home">首页</nuxt-link>
         <nuxt-link to="/product">产品页</nuxt-link>
         <div class="fr" v-if="username">
           当前用户：{{username}}
           <button @click="logout">退出</button>
         </div>
-      </header>
+      </header> -->
       <div>
         导航菜单：
         <Menu />
